cae2fafca5a385ab82aca51d5f03a5d0e552e81d
CHGHJHBB <alvarezlyonnais@outlook.fr>
2025-11-15 17:09:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -81,6 +81,6 @@ def check():
         return f"❌ Error: {e}", 500
 
 # --- Lancement du serveur ---
-if name == "__main__":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
